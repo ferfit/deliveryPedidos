4796baf3d4c371336ad9c7e4b5f78763fa6b0cd3
ferfit <ferfit16@gmail.com>
2021-08-16 13:47:53 -0200

    agregue categorias a seedCategoria

--- database/seeders/CategoriaSeeder.php
+++ database/seeders/CategoriaSeeder.php
@@ -22,5 +22,158 @@ class CategoriaSeeder extends Seeder
             'nombre'=>'Empanadas'
             
         ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Cereales'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Celiacos'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Semillas'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Almacen'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Cosmetica'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Snacks'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Leches vegetales'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Gourmet '
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Suplementos'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Bacha'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Golosinas'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Panificados'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Galletitas'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Envasados'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Dulces'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Miel'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Tes'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Aceites Acetos'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Celiacos Golosinas'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Condimentos / Salsas'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Heladera'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Freezer'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Harinas'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Varios'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Reposteria'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Mermelada'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Pastas'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Yerbas'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Edulcorantes'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Suplementos Energy'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Legumbres'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Infusiones'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Bazar'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Jugos'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Especias'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Hierbas'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Vinos'
+            
+        ]);
+        $categoria2 = Categoria::create([
+            'nombre'=>'Aromaterapia'
+            
+        ]);
+        
     }
 }
