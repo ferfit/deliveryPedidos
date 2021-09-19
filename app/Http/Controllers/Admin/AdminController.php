a1cb106e083097f33ec12eef48aa1c40ee7d1b55
Pereyra-Fernando <pereyra.r.fernando@gmail.com>
2021-09-19 12:35:00 -0200

    arregla errores index

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Orden;
+use App\Models\Producto;
+use App\Models\Categoria;
+use App\Models\User;
 
 class AdminController extends Controller
 {
     // tablero principal
     public function index(){
-        return view('admin.index');
+        $ordenes = Orden::all();
+        $productos = Producto::all();
+        $categorias = Categoria::all();
+        $usuarios = User::all();
+        return view('admin.index',compact('ordenes','productos','categorias','usuarios'));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,11 +3,141 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>Panel de administración general</h1>
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+
+    <div class="row">
+        <div class="col-lg-3 col-6">
+
+            {{-- Ordenes --}}
+            @if ($ordenes->count())
+
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>{{ $ordenes->count() }}</h3>
+
+                        <p>Ordenes</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-bag"></i>
+                    </div>
+                </div>
+
+            @else
+
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>0</h3>
+
+                        <p>Ordenes</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-bag"></i>
+                    </div>
+                </div>
+
+            @endif
+
+        </div>
+
+
+        <!-- Categorias -->
+        <div class="col-lg-3 col-6">
+
+            @if ($categorias->count())
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3>{{ $categorias->count() }}</h3>
+
+                        <p>Categorias</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                </div>
+
+            @else
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3>0</h3>
+
+                        <p>Categorias</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                </div>
+
+            @endif
+
+
+        </div>
+
+
+        <!-- Productos -->
+        <div class="col-lg-3 col-6">
+
+            @if ($productos->count())
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h3>{{ $productos->count() }}</h3>
+
+                        <p>Productos</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-pie-graph"></i>
+                    </div>
+                </div>
+            @else
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h3>0</h3>
+
+                        <p>Productos</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-pie-graph"></i>
+                    </div>
+                </div>
+
+            @endif
+
+        </div>
+
+
+
+        <!-- Usuarios -->
+
+        <div class="col-lg-3 col-6">
+            @if ($usuarios->count())
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3>{{ $usuarios->count() }}</h3>
+
+                        <p>Usuarios</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-person-add"></i>
+                    </div>
+                </div>
+            @else
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3>0</h3>
+
+                        <p>Usuarios</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-person-add"></i>
+                    </div>
+                </div>
+            @endif
+        </div>
+        <!-- ./col -->
+
+    </div>
 @stop
 
 @section('css')
@@ -15,5 +145,5 @@
 @stop
 
 @section('js')
-    
+
 @stop
